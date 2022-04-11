642191b73b4ea1e2b54f8d38728623036705ba71
MARCUIX <102602268+MarcusMattivi@users.noreply.github.com>
2022-04-11 12:19:37 -0300

    FundamentosDeDesign.java

---
+++ FundamentosDeDesign.java
@@ -0,0 +1,18 @@
+//Nessa atividade deviamos criar um docs,canva,etc falando sobre um aplicativo que voce tem costume de usar ou goste da interface e responder umas perguntas.
+RIOT GAMES
+
+
+- Quais são as cores utilizadas por ele?
+Suas cores principais são vermelho,preto,cinza e branco.
+
+- Quantos cliques e trocas de tela o usuário precisa fazer para conseguir acessar suas funcionalidades?
+Entre 1 a 3 cliques e 2 trocas de tela.
+2 funcionalidades principais são:
+1-Download de um game sendo 1 clique uma troca de tela 2x ai você faz o login ou cria uma conta e mais um clique para começar o download do jogo.
+2-Notícias:Notícias é uma aba que pode ser facilmente acessada usando apenas um clique,dentro da aba notícias qualquer notícia que o usuário clicar irá direcioná lo para outra página apenas sobre essa notícia mostrando-a completamente.
+
+- Como são os formatos? Têm um padrão? Mais arredondados ou angulados?
+São quadrados com as bordas arredondadas. 
+
+- O sistema tem acesso por outro meio (celular, desktop, web)? Você consegue visualizar que é o mesmo sistema/produto/empresa? Por que?
+Ele tem aplicativo no celular,site na web e launcher no computador.Em todas as plataformas a interface é diferente se adequando a tal meio,sendo facilmente perceptível que é a Riot Games pelo uso de cores e tendo a mesma temática em todas as plataformas.
